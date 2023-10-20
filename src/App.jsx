971d5740f3ce48a18083b9fa8b47f0cdfb25ebae
Kymani Stephens <kymanist00@gmail.com>
2023-10-20 01:30:19 -0700

    test dynamic rendering with list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import './App.css' 
+import './App.css'
+
+// const kids = ['kid1', 'kid2', 'kid3', 'kid4'] 
+const kids = []
+
+
+function List(props) {
+
+  if (!props.kids) {
+    return <p>no kids at all :/</p>
+  }
+
+  if (props.kids.length === 0) return (
+    <p>no kids here :o</p>
+  )
+
+
+  return (
+  <ul>
+    {props.kids.map((kid) => {
+      return <li key={kid}>{kid}</li>
+    })}
+
+  </ul>
+  )
+}
+
+function ListItem(props) {
+
+}
+
+
+
+
 
 function App() {
 
   return (
     <>
-    <h1>Hello this a test!</h1>
-    <h2>I'm learning REACT</h2>
-
-    </>
+    <h1>My first react APPLICATION!</h1>
+    <h2 className='test'>you dont need react for a simple website!</h2>
+    <h3>react is for applications that have dynamic information and lots of state!</h3>
+    <List kids={kids}/>
+    </> 
   )
 }
 
