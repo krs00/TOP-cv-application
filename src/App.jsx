7c5fb10a62c8f3866434048c416aa7c4905a7d9d
Kymani Stephens <kymanist00@gmail.com>
2023-10-24 14:49:38 -0700

    fix handleChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import './App.css'
+import FormInput from './components/FormInput';
 import { useState } from 'react';
 
 
 function App() {
 
-  const [value, setValue] = useState('')
+  // global object will be passed into different form components
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: ''
 
-  const handleInputChange = (e) => {
-    const newValue = e.target.value;
-    console.log(newValue) 
-    setValue(newValue)
+  })
+ 
+  function handleChange(e) {
+    // grab the name attribute of the current input field
+    const name = e.target.name
+    // grab text/value inputted into input field
+    const value = e.target.value
+    // make a copy of the form data object to avoid mutating state
+    const updatedFormData = {...formData}
+    // update key value from form data object copy with value of 
+    updatedFormData[name] = value
+    // update form data with copy
+    setFormData(updatedFormData)
   }
 
   return (
     <>
-    <h1>test</h1> 
-    </>
+
+    <FormInput labelText="Full name" />
+
+
+
+   
+    </> 
   )
 }
 
