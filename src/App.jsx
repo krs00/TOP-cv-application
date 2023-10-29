76196d2e4ecaca097ea0518e9512e02b04fcda77
Kymani Stephens <kymanist00@gmail.com>
2023-10-29 12:33:20 -0700

    update whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   function handleInputsChange(e) {
     // grab the name attribute of the current input field
-    const name = e.target.name
+    const name = e.target.name 
     // grab text/value inputted into input field
     const value = e.target.value
     // make a copy of the form data object to avoid mutating state
@@ -25,7 +25,7 @@ function App() {
     // update key value from form data object copy with value of 
     updatedFormData[name] = value
     // update form data with copy
-    setFormData(updatedFormData)
+    setFormData(updatedFormData) 
   } 
 
  
