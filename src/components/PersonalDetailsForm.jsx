c37267d88913c166fdb889608ec7a479d92761e4
Kymani Stephens <kymanist00@gmail.com>
2023-10-29 12:11:27 -0700

    update component name

---
+++ src/components/PersonalDetailsForm.jsx
@@ -0,0 +1,12 @@
+import FormInput from "./FormInput";
+
+function PersonalDetailsForm(props) {
+    return ( 
+      <> 
+       <p>personal details form</p>
+      </>
+    );
+  }
+  
+  export default PersonalDetailsForm;
+  
