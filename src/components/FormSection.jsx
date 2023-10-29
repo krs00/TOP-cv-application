df514994571233b149069314be76c2b89c865878
Kymani Stephens <kymanist00@gmail.com>
2023-10-29 12:52:39 -0700

    add import education control

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,10 +1,12 @@
 import PersonalDetailsForm from "./PersonalDetailsForm";
+import EducationControl from "./EducationControl";
 
 function FormSection(props) {
     return ( 
       <>
         <p>Form Section</p>
-        <PersonalDetailsForm handleInputsChange={props.handleInputsChange} /> 
+        <PersonalDetailsForm handleInputsChange={props.handleInputsChange} />
+        <EducationControl /> 
       </>
     );
   }
