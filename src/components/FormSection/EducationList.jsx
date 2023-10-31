90b627416f6c5716aefd2a2b257e21122f246c93
Kymani Stephens <kymanist00@gmail.com>
2023-10-31 16:44:30 -0700

    pass edit and delete props for list items

--- src/components/FormSection/EducationList.jsx
+++ src/components/FormSection/EducationList.jsx
@@ -5,7 +5,7 @@ function EducationList(props) {
       <>
         <ul className='no-list-style'>
         {props.educationList.map(item => (
-        <li key={item.id}>  <EducationListItem item={item} deleteEducation={props.deleteEducation} /> </li>
+        <li key={item.id}>  <EducationListItem toggleEducationForm={props.toggleEducationForm} item={item} deleteEducation={props.deleteEducation} toggleEditMode={props.toggleEditMode} /> </li>
         ))}
     </ul>
       
