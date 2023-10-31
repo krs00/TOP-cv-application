5ace4e46adf30847f6ddbddca289adfd93ecf874
Kymani Stephens <kymanist00@gmail.com>
2023-10-31 16:43:19 -0700

    add ability to show edit education form

--- src/components/FormSection/EducationControl.jsx
+++ src/components/FormSection/EducationControl.jsx
@@ -1,42 +1,64 @@
-import { useState } from 'react'; 
-import EducationForm from "./EducationForm"; 
+import { useState } from 'react';
+import EducationForm from "./EducationForm";
 import EducationList from "./EducationList";
+import EducationEditForm from './EducationEditForm';
 
 function EducationControl(props) {
 
-    const [isFormVisible, setFormVisible] = useState(false)
+  const [isFormVisible, setFormVisible] = useState(false)
+  const [editMode, setEditMode] = useState(false)
 
-    function toggleEducationForm() {
-      const { clearEducationData } = props
-      clearEducationData()
-      setFormVisible(!isFormVisible) 
-    }
-
-    function handleSaveBtnPress() {
-      {props.addEducation()}
-      toggleEducationForm()
-    }
-
-    if (isFormVisible === true) {
-      return ( 
-        <>
-          <EducationForm handleInputsChange={props.handleInputsChange} />
-          <button onClick={toggleEducationForm}>Cancel</button> 
-          <button onClick={handleSaveBtnPress}>Save</button>
- 
-        </>
-      ); 
-    } else if (isFormVisible === false) {
-      return ( 
-        <>
-          <EducationList 
-          educationList={props.educationList} 
-          deleteEducation={props.deleteEducation}/>
-          <button onClick={toggleEducationForm}>Add Education</button>
-        </>
-      ); 
-    }
+  function toggleEditMode() {
+    setEditMode(!editMode)
   }
-  
-  export default EducationControl;
-  
+
+  function toggleEducationForm() {
+    const { clearEducationData } = props
+    clearEducationData() 
+    setFormVisible(!isFormVisible)
+  }
+
+  function toggleEditForm() {
+    toggleEditMode()
+    toggleEducationForm()
+  }
+
+  function handleSaveBtnPress() {
+    { props.addEducation() }
+    toggleEducationForm()
+  }
+
+  if (isFormVisible === true && editMode === false) {
+    return (
+      <>
+        <EducationForm handleInputsChange={props.handleInputsChange} />
+        <button onClick={toggleEducationForm}>Cancel</button>
+        <button onClick={handleSaveBtnPress}>Save</button>
+
+      </>
+    );
+  } else if (isFormVisible === true && editMode === true) {
+
+    return (
+      <>
+      <EducationEditForm />
+      <button onClick={toggleEditForm}>go back</button>
+      </>
+    )
+
+  } else if (isFormVisible === false) {
+    return (
+      <>
+        <EducationList
+          educationList={props.educationList}
+          deleteEducation={props.deleteEducation}
+          toggleEditMode={toggleEditMode}
+          toggleEducationForm={toggleEducationForm} />
+
+        <button onClick={toggleEducationForm}>Add Education</button>
+      </>
+    );
+  }
+}
+
+export default EducationControl;
