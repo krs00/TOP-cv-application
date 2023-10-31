1b310277f88bc3177b6b7b871d909f8f0f93b138
Kymani Stephens <kymanist00@gmail.com>
2023-10-31 14:33:05 -0700

    add education list item body

--- src/components/FormSection/EducationListItem.jsx
+++ src/components/FormSection/EducationListItem.jsx
@@ -1,8 +1,20 @@
-function EducationListItem() {
+function EducationListItem(props) {
+
+    function showId(e) {
+      const btn = e.target
+      const id = btn.getAttribute('id')
+      props.deleteEducation(id) 
+    }
+
+
     return (
       <>
-
-      </>
+       <div className="education-list-item"> 
+        <p>{props.item.school}</p>
+        <button id={props.item.id} onClick={showId}>Edit</button> 
+        <button id={props.item.id} onClick={showId}>Delete</button> 
+       </div> 
+      </> 
     );
   }
   
