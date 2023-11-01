cab34b00158070bc5fd0877eff0cd30a47c857d4
Kymani Stephens <kymanist00@gmail.com>
2023-11-01 14:19:12 -0700

    pass current object as props to list items

--- src/components/FormSection/EducationList.jsx
+++ src/components/FormSection/EducationList.jsx
@@ -5,7 +5,7 @@ function EducationList(props) {
       <>
         <ul className='no-list-style'>
         {props.educationList.map(item => (
-        <li key={item.id}>  <EducationListItem currentEditId={props.currentEditId} setCurrentEditId={props.setCurrentEditId} toggleEducationForm={props.toggleEducationForm} item={item} deleteEducation={props.deleteEducation} toggleEditMode={props.toggleEditMode} /> </li>
+        <li key={item.id}>  <EducationListItem updateCurrentEditObject={props.updateCurrentEditObject} currentEditId={props.currentEditId} setCurrentEditId={props.setCurrentEditId} toggleEducationForm={props.toggleEducationForm} item={item} deleteEducation={props.deleteEducation} toggleEditMode={props.toggleEditMode} /> </li>
         ))}
     </ul>
       
