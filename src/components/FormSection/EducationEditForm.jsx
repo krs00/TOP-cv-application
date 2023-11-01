a309cceab42622f71c797462033ab812e3131ffa
Kymani Stephens <kymanist00@gmail.com>
2023-11-01 11:34:05 -0700

    create new function to handle edit update

--- src/components/FormSection/EducationEditForm.jsx
+++ src/components/FormSection/EducationEditForm.jsx
@@ -2,21 +2,26 @@ import FormInput from "./FormInput";
 
 function EducationEditForm(props) {
 
+    function handleUpdate(e) {
+      const currentEditId = props.currentEditId
+      props.handleEducationUpdate(e, currentEditId)
+    } 
+
     return ( 
       <>
         <br></br>
        <p>Edit Education Form</p>
-       <FormInput id="school" labelText="School"/> 
+       <FormInput id="school" labelText="School" handleInputsChange={handleUpdate}/> 
        <br></br>
-       <FormInput id="degree" labelText="Degree"/>
+       <FormInput id="degree" labelText="Degree" handleInputsChange={props.handleEducationUpdate}/>
        <br></br>
-       <FormInput id="eduStart" labelText="Start Date"/>
+       <FormInput id="eduStart" labelText="Start Date" handleInputsChange={props.handleEducationUpdate}/>
        <br></br>
-       <FormInput id="eduEnd" labelText="End Date"/>
+       <FormInput id="eduEnd" labelText="End Date" handleInputsChange={props.handleEducationUpdate}/>
        <br></br>
-       <FormInput id="eduLocation" labelText="Location"/>
+       <FormInput id="eduLocation" labelText="Location" handleInputsChange={props.handleEducationUpdate}/>
        <br></br>
-      </>
+      </> 
     );
   }
   
