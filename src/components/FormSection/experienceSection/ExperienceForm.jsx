b17edc86c6a44977e6be913894ac4b33ca598c70
Kymani Stephens <kymanist00@gmail.com>
2023-11-03 12:51:01 -0700

    formatting

--- src/components/FormSection/experienceSection/ExperienceForm.jsx
+++ src/components/FormSection/experienceSection/ExperienceForm.jsx
@@ -4,19 +4,12 @@ function ExperienceForm(props) {
 
     return ( 
       <>
-      
-       <p>Add Experience Form</p>
        <div className="form-container">
        <FormInput id="company" labelText="School" handleInputsChange={props.handleInputsChange}/>
-    
        <FormInput id="title" labelText="Degree" handleInputsChange={props.handleInputsChange}/>
-     
        <FormInput id="expStart" labelText="Start Date" handleInputsChange={props.handleInputsChange}/>
-      
        <FormInput id="expEnd" labelText="End Date" handleInputsChange={props.handleInputsChange}/>
-      
        <FormInput id="expLocation" labelText="Location" handleInputsChange={props.handleInputsChange}/>
-      
        <FormInput id="description" labelText="Description" handleInputsChange={props.handleInputsChange}/>
        </div>
       </>
