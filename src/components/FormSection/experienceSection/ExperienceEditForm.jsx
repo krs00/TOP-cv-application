9996e4d73be7ebf04519a102f3123bf41e45ce94
Kymani Stephens <kymanist00@gmail.com>
2023-11-03 14:49:17 -0700

    revert to text input, text area WIP

--- src/components/FormSection/experienceSection/ExperienceEditForm.jsx
+++ src/components/FormSection/experienceSection/ExperienceEditForm.jsx
@@ -14,24 +14,25 @@ function ExperienceEditForm(props) {
 
       <p className="form-modal-title">Edit education</p>
       <div className="form-container">
-        <FormInput id="company" labelText="School" handleInputsChange={handleUpdate} defaultValue={currentExperience.school} type="text" />
-        <FormInput id="title" labelText="Degree" handleInputsChange={handleUpdate} defaultValue={currentExperience.degree} type="text" />
+        <FormInput id="company" labelText="Company" handleInputsChange={handleUpdate} defaultValue={currentExperience.company} type="text" />
+        <FormInput id="title" labelText="Title" handleInputsChange={handleUpdate} defaultValue={currentExperience.title} type="text" />
         <FormInput id="expStart" labelText="Start Date" handleInputsChange={handleUpdate} defaultValue={currentExperience.expStart} type="text" />
         <FormInput id="expEnd" labelText="End Date" handleInputsChange={handleUpdate} defaultValue={currentExperience.expEnd} type="text" />
         <FormInput id="expLocation" labelText="Location" handleInputsChange={handleUpdate} defaultValue={currentExperience.expLocation} type="text" />
-        {/* <FormInput id="description" labelText="Description" handleInputsChange={handleUpdate} defaultValue={currentExperience.description} type="textarea"  /> */}
+        <FormInput id="description" labelText="Description" handleInputsChange={handleUpdate} defaultValue={currentExperience.description} type="textarea"  />
 
-        <label htmlFor="description">Description</label>
+        {/* <label htmlFor="description">Description</label>
         <div>
-          <textarea
+          <textarea 
             id="description"
             defaultValue={currentExperience.description}
+            // value={currentExperience.description}
             onChange={props.handleInputsChange}
             rows="4" // Specify the number of visible rows
             cols="30" // Specify the number of visible columns (characters)
             placeholder="Enter experience description"
           ></textarea>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- src/components/FormSection/experienceSection/ExperienceForm.jsx
+++ src/components/FormSection/experienceSection/ExperienceForm.jsx
@@ -7,23 +7,22 @@ function ExperienceForm(props) {
   return (
     <>
       <div className="form-container">
-        <FormInput id="company" labelText="School" handleInputsChange={props.handleInputsChange} />
-        <FormInput id="title" labelText="Degree" handleInputsChange={props.handleInputsChange} />
+        <FormInput id="company" labelText="Company" handleInputsChange={props.handleInputsChange} />
+        <FormInput id="title" labelText="Title" handleInputsChange={props.handleInputsChange} />
         <FormInput id="expStart" labelText="Start Date" handleInputsChange={props.handleInputsChange} />
         <FormInput id="expEnd" labelText="End Date" handleInputsChange={props.handleInputsChange} />
         <FormInput id="expLocation" labelText="Location" handleInputsChange={props.handleInputsChange} />
-        {/* <FormInput id="description" labelText="Description" handleInputsChange={props.handleInputsChange}/> */}
-        <label htmlFor="description">Description</label>
+        <FormInput id="description" labelText="Description" handleInputsChange={props.handleInputsChange}/>
+        {/* <label htmlFor="description">Description</label> 
         <div>
           <textarea
             id="description"
-            defaultValue={currentExperience.description}
             onChange={props.handleInputsChange}
             rows="4" // Specify the number of visible rows
             cols="30" // Specify the number of visible columns (characters)
             placeholder="Enter experience description"
           ></textarea>
-        </div>
+        </div> */}
       </div>
     </>
   );
